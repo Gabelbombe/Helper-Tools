8d90a5037dc45aa7b7ed61fda7cfea1d0a777fd9
ehime <dodomeki@gmail.com>
2014-06-13 14:57:27 -0700

    padding

--- imap-scrape.php
+++ imap-scrape.php
@@ -2,12 +2,12 @@
 
 Namespace MAPReader
 {
-	Class IMap
-	{
+    Class IMap
+    {
 
-		protected 	$headers 	= [],
-                    $bodies		= [],
-                    $counts		= [],
+        protected     $headers     = [],
+                    $bodies        = [],
+                    $counts        = [],
                     $mb         = null;
 
         public      $host       = false,
@@ -15,9 +15,9 @@ Namespace MAPReader
                     $user       = false,
                     $pass       = false;
 
-		public function __construct($host, $port, $user, $pass)
-		{
-			foreach (['host','port','user','pass'] AS $key)
+        public function __construct($host, $port, $user, $pass)
+        {
+            foreach (['host','port','user','pass'] AS $key)
             {
                 $this->$key = $key; // breaks when parent constructs overriding children ....
 
@@ -27,7 +27,7 @@ Namespace MAPReader
                     break;
                 }
             }
-		}
+        }
 
         /**
          * Open IMap loop
@@ -35,11 +35,11 @@ Namespace MAPReader
          * @return $this
          */
         public function open()
-		{
-			$this->mb = imap_open("{$this->host}:{$this->port}/imap", $this->user, $this->pass);
+        {
+            $this->mb = imap_open("{$this->host}:{$this->port}/imap", $this->user, $this->pass);
 
-				return $this;
-		}
+                return $this;
+        }
 
         /**
          * Close loop
@@ -57,17 +57,17 @@ Namespace MAPReader
          * @return $this
          */
         public function read()
-		{
-			$this->counts[] = imap_num_msg($this->mb);
+        {
+            $this->counts[] = imap_num_msg($this->mb);
 
-			for ($c=1;$c<=end($this->counts);$c++)
-			{
-			   $this->headers[] = imap_headerinfo($this->mb, $c);
-			   $this->bodies[]  = imap_fetchbody($this->mb, $c, 1);
-			}
+            for ($c=1;$c<=end($this->counts);$c++)
+            {
+               $this->headers[] = imap_headerinfo($this->mb, $c);
+               $this->bodies[]  = imap_fetchbody($this->mb, $c, 1);
+            }
 
-				return $this;
-		}
+                return $this;
+        }
 
         /**
          * Process an individual or a batch
@@ -76,22 +76,22 @@ Namespace MAPReader
          * @return $this
          */
         public function process($enum=0)
-		{
-			if (is_int($enum) && isset($this->headers[$enum]) && isset($this->bodies[$enum]))
-			{
-				$this->doSomething($this->headers[$enum], $this->bodies[$enum]);
-			}
+        {
+            if (is_int($enum) && isset($this->headers[$enum]) && isset($this->bodies[$enum]))
+            {
+                $this->doSomething($this->headers[$enum], $this->bodies[$enum]);
+            }
 
-			if ('batch' === strtolower($enum) && count($this->headers) === count($this->bodies))
-			{
-				for ($i=0;$i<count($this->headers);$i++)
-				{
-					$this->doSomething($this->headers[$i], $this->bodies[$i]);
-				}
-			}
+            if ('batch' === strtolower($enum) && count($this->headers) === count($this->bodies))
+            {
+                for ($i=0;$i<count($this->headers);$i++)
+                {
+                    $this->doSomething($this->headers[$i], $this->bodies[$i]);
+                }
+            }
 
             return $this;
-		}
+        }
 
         /**
          * Should do something with Headers and Body
@@ -101,10 +101,10 @@ Namespace MAPReader
          * @return string
          */
         private function doSomething(array $headers, $bodies)
-		{
-			return 'wat-do?';
-		}
-	}	
+        {
+            return 'wat-do?';
+        }
+    }    
 }
 
 
